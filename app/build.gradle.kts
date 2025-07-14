c3d21647fc6428b78a176d6c68ae0ef73fe351a7
H-Shamkhani <shamkhanihossein93@gmail.com>
2025-07-14 17:20:30 +0330

    Correct jdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,12 +30,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlin {
         compilerOptions {
-            jvmTarget = JvmTarget.JVM_17
+            jvmTarget = JvmTarget.JVM_11
         }
     }
     buildFeatures {
